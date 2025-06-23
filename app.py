9bf40bd9a4a5cbc125c6ee255a9716b3d13d7592
M Ridho NF <ridhoamstrong4@gmail.com>
2025-06-23 21:30:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ try:
             # Menampilkan hasil prediksi ke user
         st.success(f"Prediksi permintaan untuk tahun {tahun_pred}: {prediksi:.0f} unit")
             
-        st.success(f"Total Biaya Untuk Tahun {tahun_pred} : Rp. {final_cost}")
+        st.success(f"Total Biaya Untuk Tahun {tahun_pred} : Rp. {final_cost:.0f}")
 
             # ==========================
             # Visualisasi Garis Regresi
