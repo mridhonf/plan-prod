c2c1668954648fbac96b497ccad942ccefbca263
M Ridho NF <ridhoamstrong4@gmail.com>
2025-06-23 21:09:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -77,8 +77,7 @@ try:
 
             # Menandai hasil prediksi dengan garis horizontal
         ax.axhline(prediksi, color='green', linestyle=':', label='Prediksi')
-            # Menambahkan Harga
-        ax.plot(final_cost, label='Biaya')
+            
 
             # Memberi label sumbu dan legend
         ax.set_xlabel("Tahun")
